db394a1da095615ad10d1e2433a12a184cea7616
Pengfei Zhao <p.zhao.6174@gmail.com>
2023-02-19 21:36:07 +0100

    add main (#5)

--- src/nerf/__init__.py
+++ src/nerf/__init__.py
@@ -1,4 +1,3 @@
-from nerf.ray_helpers import get_ray_bundle, sample_query_points, positional_encoding
 import importlib.metadata
 
-__version__ = importlib.metadata.version("mypackage")
+__version__ = importlib.metadata.version("jax-nerf")
